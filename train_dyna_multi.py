9bc57e4ddd0ec283f2fac0fb859eae9d1a3a10d2
linzhank <linzhank@gmail.com>
2020-04-06 15:12:03 -0400

    episode logging step count; crop pe_env.png

--- train_dyna_multi.py
+++ train_dyna_multi.py
@@ -120,7 +120,7 @@ if __name__ == '__main__':
         # sedimentary returns
         sedimentary_returns_p[ep] = np.sum(episodic_returns_p[:ep+1],axis=0)/(ep+1)
         sedimentary_returns_e[ep] = np.sum(episodic_returns_e[:ep+1],axis=0)/(ep+1)
-        logging.info("\n===\nepisode: {} \nepisodic_returns: {} \nsedimentray_returns: {} \n===\n".format(ep+1, (episodic_returns_p[ep], episodic_returns_e[ep]), (sedimentary_returns_p[ep], sedimentary_returns_e[ep])))
+        logging.info("\n===\nepisode: {} \ngame lasts: {} steps \nepisodic_returns: {} \nsedimentray_returns: {} \n===\n".format(ep+1, st+1, (episodic_returns_p[ep], episodic_returns_e[ep]), (sedimentary_returns_p[ep], sedimentary_returns_e[ep])))
         # uncomment following to plot episodic average returns, but will slow down training
         ###################################################################################
         # fig_r.clf()
