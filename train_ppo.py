207a49478a913686a652a4cbcdeb515cf46eae26
linzhank <linzhank@gmail.com>
2020-06-17 19:07:26 -0400

    add compute_odometry

--- train_ppo.py
+++ train_ppo.py
@@ -14,7 +14,7 @@ print(tf.__version__)
 import tensorflow_probability as tfp
 tfd = tfp.distributions
 
-from pe_env import PursuitEvasion
+from envs.pe_env import PursuitEvasion
 
 ################################################################
 """
@@ -34,16 +34,64 @@ class Actor(tf.Module):
             logp_a = self._log_prob_from_distribution(pi, act)
         return pi, logp_a
 
-class GaussianActor(Actor):
-    def __init__(self):
-        pass
+class ActorCritic(tf.Module):
+    def __init__(self, obs_dim, act_dim, hidden_sizes=(64,64), activation='tanh'):
+        super().__init__()
+        self.actor = MLPGaussianActor(obs_dim=obs_dim, act_dim=act_dim, hidden_sizes=hidden_sizes, activation=activation)
+        self.critic = MLPCritic(obs_dim=obs_dim, hidden_sizes=hidden_sizes, activation=activation)
+
+    # @tf.function
+    def step(self, obs):
+        with tf.GradientTape() as t:
+            with t.stop_recording():
+                pi_dist = self.actor._distribution(obs)
+                a = pi_dist.sample()
+                logp_a = self.actor._log_prob_from_distribution(pi_dist, a)
+                v = self.critic(obs)
+
+        # return a.numpy(), v.numpy(), logp_a.numpy()
+        return a, v, logp_a
+
+    # @tf.function
+    def act(self, obs):
+        return self.step(obs)[0] 
+
+def compute_odometry(env):
+    """
+    compute odometries of all players
+    Args:
+        env: PE environment
+    Returns:
+        odoms: (x, y, vx, vy)*num_players
+    """
+    num_e = env.num_evaders
+    num_p = env.num_pursuers
+    odoms = np.zeros((num_e+num_p,4)) # x,y,vx,vy
+    for ie in range(num_e):
+        odoms[ie] = np.concatenate((env.evaders['position'][ie], env.evaders['velocity'][ie]), axis=0)
+    for ip in range(num_p):
+        odoms[-num_p+ip] = np.concatenate((env.pursuers['position'][ip], env.pursuers['velocity'][ip]), axis=0)
+
+    return odoms
 
 
+# Main
+# parameters
 epochs = 4
-steps_per_epoch = 100
-if __name__='__main__':
-    env=PursuitEvasion()
-    obs, ep_ret, ep_len = env.reset(), 0, 0
-    for ep in range(epoch):
-        for st in range(steps_per_epoch):
-            pass
+steps_per_epoch = 100 # make sure this can cover several episodes
+# prepare
+env=PursuitEvasion()
+obs, ep_ret, ep_len = env.reset(), 0, 0 # obs is map
+num_evaders = env.num_evaders
+num_pursuers = env.num_pursuers
+acts = np.zeros((num_evaders+num_pursuers,2))
+vals = np.zeros(num_evaders+num_pursuers)
+logps = np.zeros(num_evaders+num_pursuers)
+odoms = compute_odometry(env)
+#     for ep in range(epochs):
+#         for st in range(steps_per_epoch):
+#             # compute evaders action
+#             for ie in range(num_evaders):
+#                 if not done[ie]:
+#                     acts[ie], vals[ie], logps[ie] = agent_eva.step(obs, odoms[ie])
+
