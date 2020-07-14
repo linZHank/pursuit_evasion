62315427c91def7fcf3516df2dafc2daea126653
linzhank <linzhank@gmail.com>
2020-07-14 16:50:49 -0400

    increase target KL

--- train_1v1_pur_ppo.py
+++ train_1v1_pur_ppo.py
@@ -99,14 +99,14 @@ class PPOBuffer:
 if __name__=='__main__':
     # instantiate env
     env = PursuitEvasionOneVsOneContinuous(resolution=(80,80))
-    agent = PPOAgent(name='ppo_train', dim_img=(80,80,4), lr_actor=1e-4, lr_critic=3e-4, batch_size=128, target_kl=0.2)
+    agent = PPOAgent(name='ppo_train', dim_img=(80,80,4), lr_actor=3e-4, lr_critic=1e-3, batch_size=128, target_kl=0.5)
     model_dir_actor = os.path.join(sys.path[0], 'saved_models', env.name, agent.name, 'models', 'actor/')
     model_dir_critic = os.path.join(sys.path[0], 'saved_models', env.name, agent.name, 'models', 'critic/')
     # parameter
     num_episodes = 20000
     num_steps = env.max_episode_steps
-    buffer_size = int(2e5)
-    update_every = 200
+    buffer_size = int(3e5)
+    update_every = 300
     # variables
     step_counter = 0
     episode_counter = 0
