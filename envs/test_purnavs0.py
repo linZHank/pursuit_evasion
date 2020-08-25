0726e275bca3ed582a235026d4023df7f346356b
linzhank <linzhank@gmail.com>
2020-08-25 19:04:40 -0400

    update purnavs0 with dense(distance) reward

--- envs/test_purnavs0.py
+++ envs/test_purnavs0.py
@@ -1,23 +1,23 @@
 import numpy as np
 import cv2
-from purnav_scene0 import PursuerNavigationScene0
+from purnavs0 import PursuerNavigationScene0
 if __name__ == '__main__':
     env=PursuerNavigationScene0()
     for ep in range(10):
-        obs = env.reset()
+        o = env.reset()
         # cv2.imshow('map', cv2.resize(obs[:,:,[2,1,0]], (640, 640)))
         # if cv2.waitKey(25) & 0xFF == ord('q'):
         #     break
         # env.render(pause=2)
         for st in range(env.max_episode_steps):
-            cv2.imshow('map', cv2.resize(obs[:,:,[2,1,0]], (640, 640)))
+            cv2.imshow('map', cv2.resize(o[:,:,[2,1,0]], (640, 640)))
             if cv2.waitKey(25) & 0xFF == ord('q'):
                 break
             env.render(pause=1./env.rate)
-            action = np.random.uniform(-4,4,size=2)
-            obs, rew, done, info = env.step(action)
-            print("\nepisode: {} \nstep: {} \nevaders_pos: {} \npursuers_pos: {} \nreward: {} \ndone: {}".format(ep+1, st+1, env.evaders['position'], env.pursuers['position'], rew, done))
-            if info:
-                print(info)
+            a = np.random.randint(0,env.action_reservoir.shape[0])
+            o, r, d, i = env.step(a)
+            print("\nepisode: {} \nstep: {} \nevaders_pos: {} \npursuers_pos: {} \nreward: {} \ndone: {}".format(ep+1, st+1, env.evaders['position'], env.pursuers['position'], r, d))
+            if i:
+                print(i)
                 break
     cv2.destroyAllWindows()
